d7293956456b961c575c532d7da4850db6fbcb69
Sumant Sapra <sumant.sapra@gmail.com>
2020-10-16 02:11:45 +1100

    updated the form validation javascript

--- ui.frontend/src/main/webpack/components/_form-validations.js
+++ ui.frontend/src/main/webpack/components/_form-validations.js
@@ -2,28 +2,91 @@
 // This script logs the value of the component's text property model message to the console
 
 (function() {
-	
-	function validateemail() {
-		var email = document.getElementById("wknd--membership__email");
-        email.addEventListener("blur", function(e){
-             if(/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email.value)){
-                email.removeAttribute("style");
-             }else{
-                 email.style.border = "1px solid red";
-				 email.style.background="transparent url('../resources/images/Warning.png') 0% 0% no-repeat padding-box;";
-				 var node = document.createElement("div");
-				 node.className += "error__message";
-				 var textnode = document.createTextNode(email.parentElement.getAttribute('data-cmp-constraint-message'));
-				 node.appendChild(textnode);
-				 email.appendChild(node);
-             }
-
-        })
+	function addErrorNode(elementName, elementID) {
+		elementName.style.border = "1px solid red";
+		var node = document.createElement("div");
+		node.className += "error__message";
+		node.id = elementID;
+		var textnode = "";
+		if ( elementName.value.length < 1 ) {
+			textnode = document.createTextNode(elementName.parentElement.getAttribute('data-cmp-required-message'));
+		} else {
+			textnode = document.createTextNode(elementName.parentElement.getAttribute('data-cmp-constraint-message'));
+		}
+		node.appendChild(textnode);
+		var element =  document.getElementById(elementID);
+		if (typeof(element) != 'undefined' && element != null) {
+		  // Exists.
+		} else {
+		 	elementName.parentElement.appendChild(node);
+		 	elementName.className += " error__icon";
+		}
 	}
 	
-    function onDocumentReady() {
-        validateemail();
+	function removeErrorNode(elementName, elementID) {
+		var hashElementId = "#" + elementID;
+		elementName.classList.remove("error__icon");
+		if (elementName.parentElement.querySelector(hashElementId)) {
+			var node = document.getElementById(elementID);
+			elementName.parentElement.removeChild(node);
+		}
+	}
+	
+	function validateEmail() {
+		var email = document.getElementById("wknd--membership__email");
+        email.addEventListener("blur", function(e){
+        	removeErrorNode(email, 'wknd--membership__email-id');
+             if((email.value.length > 0) &&  (/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email.value))){
+                email.removeAttribute("style");
+             }else{
+                 addErrorNode(email, 'wknd--membership__email-id');
+             }
 
+        });
+	}
+	
+	function validateName() {
+		var name = document.getElementById("wknd--membership__name");
+		name.addEventListener("blur", function(e){
+		removeErrorNode(name, 'wknd--membership__name-id');
+			if(name.value.length > 0 ) {
+				name.removeAttribute("style");
+			} else {
+				addErrorNode(name, 'wknd--membership__name-id');
+			}
+		});
+	}
+	
+	function validatePhone() {
+		var phone = document.getElementById("wknd--membership__phone");
+		phone.addEventListener("blur", function(e){
+		removeErrorNode(phone, 'wknd--membership__phone-id');
+			if(phone.value.length == 10 && (isNaN(phone.value)==false)) {
+				phone.removeAttribute("style");
+			} else {
+				addErrorNode(phone, 'wknd--membership__phone-id');
+			}
+		});
+	}
+	
+	function validateCard() {
+		var card = document.getElementById("wknd--membership__card");
+		card.addEventListener("blur", function(e){
+		removeErrorNode(card, 'wknd--membership__card-id');
+			if(card.value.length == 16) {
+				card.removeAttribute("style");
+			} else {
+				addErrorNode(card, 'wknd--membership__card-id');
+			}
+		});
+	}
+
+    function onDocumentReady() {
+        validateEmail();
+        validateName();
+        validatePhone();
+        validateCard();
+        enableButton();
     }
 
     if (document.readyState !== "loading") {
